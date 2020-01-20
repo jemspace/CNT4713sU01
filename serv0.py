d02b864813662d0e9c2b04f9727c7ab142596c49
jemspace <49767794+jemspace@users.noreply.github.com>
2020-01-19 23:47:07 -0500

    Add files via upload

---
+++ serv0.py
@@ -0,0 +1,15 @@
+# Chepurna
+# ID:6152714
+# CNT4713-U01 - assignment 2
+# 1/19/2020
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port",PORT)
+    httpd.serve_forever()
