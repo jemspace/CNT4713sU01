f4f332e81026c0271804e849d2cdb56e963d1f5f
jemspace <49767794+jemspace@users.noreply.github.com>
2020-01-22 14:21:55 -0500

    Update serv0.py

--- serv0.py
+++ serv0.py
@@ -11,5 +11,4 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port",PORT)
     httpd.serve_forever()
